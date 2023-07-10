d94846dfce674e53ae8096e73c2088c6fa8b2847
sejineeee <sejinee0918@gmail.com>
2023-07-10 23:56:46 +0900

    chore: eslint 설정

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "plugin:react/recommended",
+        "standard-with-typescript"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "eqeqeq": "error",
+        "no-unused-vars": "error",
+        "no-alert": "error",
+        "no-var": "error",
+        "no-multiple-empty-lines": "error",
+        "no-console": ["warn", { "allow": ["warn", "error"]}],
+
+    }
+}
