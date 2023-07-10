0b9889c69ab44032be767935d5eabf8d6ff4d1fe
sejineeee <sejinee0918@gmail.com>
2023-07-10 23:56:03 +0900

    chore: webpack config 설정

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src/index.tsx'),
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: 'ts-loader',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '...']
+    },
+    devServer: {
+        historyApiFallback: {
+            index: 'index.html',
+        }
+    },
+}
