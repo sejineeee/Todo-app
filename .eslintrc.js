63e33fc74a868a1d499e1f7865a99969f5df8706
sejineeee <sejinee0918@gmail.com>
2023-07-15 23:49:06 +0900

    chore: parser, settings, rule 설정 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,20 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'standard-with-typescript'],
-  overrides: [],
+  extends: ['plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
+    parser: '@typescript-eslint/parser',
+    project: './tsconfig.json',
   },
-  plugins: ['react'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['react', '@typescript-eslint'],
   rules: {
     eqeqeq: 'error',
     'no-unused-vars': 'error',
@@ -17,5 +24,9 @@ module.exports = {
     'no-var': 'error',
     'no-multiple-empty-lines': 'error',
     'no-console': ['warn', { allow: ['warn', 'error'] }],
+    '@typescript-eslint/semi': 'off',
+    '@typescript-eslint/member-delimiter-style': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
   },
 };
