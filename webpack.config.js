ce57cc0c0d97ef416e11b6fe0096e82734292607
sejineeee <sejinee0918@gmail.com>
2023-07-15 23:36:58 +0900

    chore: use 설정 추가 및 format

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    entry: path.resolve(__dirname, 'src/index.tsx'),
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                exclude: /node_modules/,
-                use: 'ts-loader',
-            },
-        ],
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.tsx'),
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: ['ts-loader', 'babel-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '...'],
+  },
+  devServer: {
+    historyApiFallback: {
+      index: 'index.html',
     },
-    resolve: {
-        extensions: ['.ts', '.tsx', '...']
-    },
-    devServer: {
-        historyApiFallback: {
-            index: 'index.html',
-        }
-    },
-}
+  },
+};
