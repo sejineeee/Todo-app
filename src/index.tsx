f09d41eb33dae765418a15d5a877cf75c2f85bf2
sejineeee <sejinee0918@gmail.com>
2023-07-18 15:18:19 +0900

    feat: redux 사용을 위해 provider 적용
    redux 사용을 위해 provider 적용
    document HTMLElement type 에러로 인해 Element로 수정

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { Provider } from 'react-redux';
+
+import { store } from './store/store';
+
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('app') as HTMLElement);
+const root = ReactDOM.createRoot(document.getElementById('app') as Element);
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+);
