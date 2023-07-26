c7318485757bd54cdd2802e303ea7e929bc13813
sejineeee <sejinee0918@gmail.com>
2023-07-26 12:26:38 +0900

    chore: module rules 추가
    css, sass 사용을 위한 rules 추가

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,14 @@ module.exports = {
         exclude: /node_modules/,
         use: ['ts-loader', 'babel-loader'],
       },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
     ],
   },
   resolve: {
