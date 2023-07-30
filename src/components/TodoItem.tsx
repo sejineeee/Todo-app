77fa1959b6a5763cb44c434e07e6b9e8320f7c72
sejineeee <sejinee0918@gmail.com>
2023-07-30 22:03:05 +0900

    feat: date picker 적용 및 수정
    react date picker 적용
    useState type 설정
    createdDate를 date picker로 대체할 계획이기 때문에 props 제거

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,4 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
 import Button from './common/Button';
 import { useAppDispatch } from '../store/hooks';
 import { deleteTodoItem } from '../store/reducer';
@@ -6,19 +10,38 @@ import { deleteTodoItem } from '../store/reducer';
 interface TodoItemProps {
   id: number;
   content: string;
-  createdDate: string;
 }
 
-const TodoItem = ({ id, content, createdDate }: TodoItemProps): JSX.Element => {
+const TodoItem = ({ id, content }: TodoItemProps): JSX.Element => {
   const dispatch = useAppDispatch();
 
   const handleClick = (id: number): void => {
     dispatch(deleteTodoItem(id));
   };
+
+  const [startDate, setStartDate] = useState<Date | null>(new Date());
+  const [endDate, setEndDate] = useState<Date | null>(new Date());
+
   return (
     <div>
+      <div>
+        <DatePicker
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+          selectsStart
+          startDate={startDate}
+          endDate={endDate}
+        />
+        <DatePicker
+          selected={endDate}
+          onChange={(date) => setEndDate(date)}
+          selectsEnd
+          startDate={startDate}
+          endDate={endDate}
+          minDate={startDate}
+        />
+      </div>
       <p>{content}</p>
-      <p>{createdDate}</p>
       <Button type="button" content="삭제" onClick={() => handleClick(id)} />
     </div>
   );
